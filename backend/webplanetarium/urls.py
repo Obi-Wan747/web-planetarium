d8c83ecc61f8cc0447dc0e142c6fa5deb1068081
Saumya Singhal <saumyasmathtutoring@gmail.com>
2024-07-21 21:20:59 -0700

    example of how to add endpoints

--- backend/webplanetarium/urls.py
+++ backend/webplanetarium/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("api", views.helloapi, name="helloapi"),
 ]
