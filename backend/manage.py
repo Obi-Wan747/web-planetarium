8f51d72eb626e08e8feb8d01a6cffd298f77806f
saumyasinghal747 <saumyas4@illinois.edu>
2024-08-11 22:09:15 -0700

    read PORT env variable

--- backend/manage.py
+++ backend/manage.py
@@ -7,6 +7,11 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webplanetarium.settings')
+    
+    # Override default port for `runserver` command
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = os.getenv("PORT")
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
